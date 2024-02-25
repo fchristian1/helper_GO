449584442c20097fee9915d2b987a0a60df39479
fc1 <fchristian1@gmx.net>
2024-02-25 22:39:36 +0100

    25022024

--- uuid.go
+++ uuid.go
@@ -1,6 +1,10 @@
 package helper_GO
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func UUID_NewString() string {
 	return uuid.New().String()
@@ -10,8 +14,9 @@ func UUID_NameNewString(name string) string {
 	return name + "_" + uuid.New().String()
 }
 
-//UUID validation
+// UUID validation
 func UUID_Validate(uuidString string) bool {
 	err := uuid.Validate(uuidString)
+	fmt.Println(err)
 	return err == nil
 }
